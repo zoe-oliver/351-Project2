3a396092e064b4e7686218552010b558f4b30074
zoeoli02 <56969730+zoeoli02@users.noreply.github.com>
2022-11-03 17:41:14 -0400

    added code for reading and writing to pipe

--- server.c
+++ server.c
@@ -18,55 +18,97 @@ int main (int argc, char **argv)
  char buf[MAXLINE];
  struct sockaddr_in cliaddr, servaddr;
 
+ int fd[2];
+ pipe(fd);
+
+ int id = fork();
+
+  /*check if it is a parent or a child */
+
+ if(id == 0){
+    //child reading from pipe
+    close(fd[1]);
+    char concat_str[100];
+
+    read(fd[0], concat_str, 100);
+
+    int k = strlen(concat_str);
+    int i;
+    for (i = 0; i < strlen(fixed_str); i++) {
+      concat_str[k++] = fixed_str[i];
+    }
+    //concat_str[k] = '\0';
+
+    close(fd[0]);
+    close(fd[1]);
+
+    //child writing to pipe
+    write(fd[1], concat_str, strlen(concat_str) + 1); //check for error buf + 1, might be wrong length
+    close(fd[1]);
+ } else {
+    //writing to pipe
+    char concat_str[100];
+    close(fd[0]);
+
+    write(fd[1], buf, strlen(buf) + 1); //check for error buf + 1, might be wrong length
+    close(fd[1]);
+    wait(NULL);
+    close(fd[1]);
+
+    read(fd[1], concat_str, 100);
+    printf("Concatenated string %s\n", concat_str);
+    close(fd[0]);
+ }
+
  //Create a socket for the server
  //If sockfd<0 there was an error in the creation of the socket
- if ((listenfd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
-  perror("Problem in creating the socket");
-  exit(2);
- }
+//  if ((listenfd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
+//   perror("Problem in creating the socket");
+//   exit(2);
+//  }
 
 
- //preparation of the socket address
- servaddr.sin_family = AF_INET;
- servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
- servaddr.sin_port = htons(SERV_PORT);
+//  //preparation of the socket address
+//  servaddr.sin_family = AF_INET;
+//  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+//  servaddr.sin_port = htons(SERV_PORT);
 
- //bind the socket
- bind (listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+//  //bind the socket
+//  bind (listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
- //listen to the socket by creating a connection queue, then wait for clients
- listen (listenfd, LISTENQ);
+//  //listen to the socket by creating a connection queue, then wait for clients
+//  listen (listenfd, LISTENQ);
 
- printf("%s\n","Server running...waiting for connections.");
+//  printf("%s\n","Server running...waiting for connections.");
 
- for ( ; ; ) {
+//  for ( ; ; ) {
 
-  clilen = sizeof(cliaddr);
-  //accept a connection
-  connfd = accept (listenfd, (struct sockaddr *) &cliaddr, &clilen);
+//   clilen = sizeof(cliaddr);
+//   //accept a connection
+//   connfd = accept (listenfd, (struct sockaddr *) &cliaddr, &clilen);
 
-  printf("%s\n","Received request...");
+//   printf("%s\n","Received request...");
 
-  if ( (childpid = fork ()) == 0 ) {//if itâ€™s 0, itâ€™s child process
+//   if ( (childpid = fork ()) == 0 ) {//if itâ€™s 0, itâ€™s child process
 
-    printf ("%s\n","Child created for dealing with client requests");
+//     printf ("%s\n","Child created for dealing with client requests");
 
-    //close listening socket
-    close (listenfd);
+//     //close listening socket
+//     close (listenfd);
 
-    while ( (n = recv(connfd, buf, MAXLINE,0)) > 0)  {
-      printf("%s","String received from and resent to the client:");
-      puts(buf);
-      send(connfd, buf, n, 0);
-    }
+//     while ( (n = recv(connfd, buf, MAXLINE,0)) > 0)  {
+//       printf("%s","String received from and resent to the client:");
+//       puts(buf);
+//       send(connfd, buf, n, 0);
+//     }
 
-    if (n < 0)
-      printf("%s\n", "Read error");
-    exit(0);
-  }
- //close socket of the server
- close(connfd);
+//     if (n < 0)
+//       printf("%s\n", "Read error");
+//     exit(0);
+//   }
+//  close socket of the server
+//  close(connfd);
  
- }
+//  }
 
 }
